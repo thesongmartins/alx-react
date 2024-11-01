e6eb26cbac425d82d0419f90f69d911483acd420
thesongmartins <martinsjehoshaphat@gmail.com>
2024-11-01 20:27:20 +0100

    updated

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -2,8 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// src/setupTests.js
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
+import { TextEncoder, TextDecoder } from 'util';
+// Polyfill TextEncoder and TextDecoder
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 
 configure({ adapter: new Adapter() });
